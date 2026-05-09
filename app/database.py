f22d87de5eb1027d6cab3d4b8605af4613da329e
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2026-05-09 14:37:32 -0500

    docker files added

--- app/database.py
+++ app/database.py
@@ -7,10 +7,14 @@ import os
 
 load_dotenv()
 
-sqlite_url = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL")
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+if database_url and database_url.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+
+engine = create_engine(database_url, connect_args=connect_args)
 
 
 def create_db_and_tables():
