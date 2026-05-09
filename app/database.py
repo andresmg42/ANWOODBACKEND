354bebfdaee1dc537c64522ba67e49fa322b4338
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2026-05-09 13:58:39 -0500

    unused imports deleted and relational schema diagram added

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
@@ -17,10 +16,10 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
 def get_session():
     with Session(engine) as session:
         yield session
-
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
