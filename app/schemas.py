e02874213e7764b8f6afdd5852b625c4191f313c
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2026-09-10 09:49:10 -0500

    cors udptated

--- app/schemas.py
+++ app/schemas.py
@@ -4,7 +4,7 @@ from typing import Optional
 from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 
-from .types import ApiDecimal
+from .decimal_types import ApiDecimal
 
 
 class Token(BaseModel):
